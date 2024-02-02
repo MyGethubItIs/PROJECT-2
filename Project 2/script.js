822ebf34f3a9c23ff816e9dd02d51ae86af94fa4
MyGethubItIs <158566893+MyGethubItIs@users.noreply.github.com>
2024-02-02 17:55:06 +0530

    Add files via upload

---
+++ Project 2/script.js
@@ -0,0 +1,94 @@
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+function firstPageAnim(){
+var t1=gsap.timeline();
+
+t1.from("#nav",{
+y:'-10',
+opacity:0,
+duration:1.5,
+ease:Expo.easeInOut
+})
+
+.to(".boundingelem",{
+y:0,
+ease:Expo.easeInOut,
+duration:2,
+delay:-1,
+Stagger:.2
+})
+
+.from("#herofooter",{
+y:-10,
+opacity:0,
+duration:1.5,
+delay:-1,
+ease:Ease.easeInOut
+})
+}
+
+
+
+
+
+function circleChaptakaro(){
+//define default scale value
+var xscale=1;
+var yscale=1;
+
+var xprev=0;
+var yprev=0;
+
+window.addEventListener("mousemove",function(dets){
+
+    xscale=gsap.utils.clamp(.8,1.2,dets.clientX-xprev);
+    yscale=gsap.utils.clamp(.8,1.2,dets.clientY-yprev);
+
+xprev=dets.clientX;
+yprev=dets.clientY;
+
+circleMousefollower(xscale,yscale);
+
+});
+
+}
+
+
+circleChaptakaro();
+
+function circleMousefollower(xscale,yscale){
+window.addEventListener("mousemove",function(dets){
+
+document.querySelector("#minicircle").style.transform=`translate(${dets.clientX}px,${dets.clientY}px) scale(${xscale},${yscale})`;
+
+})
+}
+circleMousefollower();
+firstPageAnim();
+
+document.querySelectorAll(".elem").forEach(function(elem){
+
+var rotate=0;
+var diffrot=0;
+elem.addEventListener("mousemove",function (dets){
+    var diff=dets.clientY-elem.getBoundingClientRect().top;
+
+    diffrot=dets.clientX-rotate;
+    rotate=dets.clientX;
+    gsap.to(elem.querySelector("img"),{
+    opacity:1,
+    ease:Power1,
+    top:diff,
+    left:dets.clientX,
+    rotate:gsap.utils.clamp(-20,20,diffrot),
+    });
+
+});
+
+
+});
